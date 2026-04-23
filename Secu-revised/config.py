c86aaf38b7d34cb708f28856c026a7cb2bc9b1f2
adawang <adawang12101210@gmail.com>
2026-04-23 19:30:46 +0800

    config: 分群結果輸出到 output 資料夾，避免污染訓練資料

--- Secu-revised/config.py
+++ Secu-revised/config.py
@@ -1,6 +1,6 @@
 import os
 
-folder_path = r'.\data\train'
+folder_path = r'.\output'
 result_Path = r'.\result'
-clusters_amount = 12
+clusters_amount = 9
 subfolder_path = os.path.join(folder_path, "cluster" + str(clusters_amount))
